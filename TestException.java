77ecaaa1aa679e0e282169d91b8a2330a44424b6
samxara <35997471+samxara@users.noreply.github.com>
2019-10-02 12:36:31 +0400

    Add files via upload

---
+++ TestException.java
@@ -0,0 +1,27 @@
+package com;
+
+public class TestException {
+    public void exception1(){
+        int x = 12;
+        try {
+            System.out.println("Before");
+            System.out.println(x/0);
+            System.out.println("After");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("Finaly block");
+        }
+        // int [] m = {1,2,3,4,5};
+        // int y = m[17];
+        // System.out.print(y);
+    }
+
+    public void exception2(){
+        Thread.sleep(l:3000);
+        
+    }
+
+}
